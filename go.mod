4388abe6b87e8f875c6cc3c3acd3eb27c176f492
Christopher Lamm <chris@theantichris.com>
2020-03-19 07:54:51 -0400

    setup server

--- go.mod
+++ go.mod
@@ -3,8 +3,9 @@ module github.com/theantichris/go-api-template
 go 1.14
 
 require (
-	github.com/getsentry/sentry-go v0.5.1 // indirect
-	github.com/gorilla/mux v1.7.4 // indirect
-	github.com/joho/godotenv v1.3.0 // indirect
-	github.com/sirupsen/logrus v1.4.2 // indirect
+	github.com/getsentry/sentry-go v0.5.1
+	github.com/gorilla/mux v1.7.4
+	github.com/joho/godotenv v1.3.0
+	github.com/sirupsen/logrus v1.4.2
+	golang.org/x/net v0.0.0-20190827160401-ba9fcec4b297
 )
---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/getsentry/sentry-go"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	log "github.com/sirupsen/logrus"
+	"golang.org/x/net/context"
+)
+
+func main() {
+	godotenv.Load()
+
+	dsn := os.Getenv("SENTRY_DSN")
+	if dsn != "" {
+		loadSentry(dsn)
+		defer sentry.Flush(time.Second * 2)
+	}
+
+	router := mux.NewRouter()
+
+	server := &http.Server{
+		Addr:         "0.0.0.0:" + os.Getenv("PORT"),
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+		Handler:      router,
+	}
+
+	go serve(server)
+
+	interruptChan := make(chan os.Signal, 1)
+	signal.Notify(interruptChan, os.Interrupt)
+	<-interruptChan
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*15)
+	defer cancel()
+
+	server.Shutdown(ctx)
+	log.Infoln("shutting down")
+	os.Exit(0)
+}
+
+func loadSentry(dsn string) {
+	if err := sentry.Init(sentry.ClientOptions{Dsn: dsn}); err != nil {
+		log.Errorln("failed to connect to Sentry: ", err)
+	} else {
+		log.Infoln("connected to Sentry")
+	}
+}
+
+func serve(server *http.Server) {
+	if err := server.ListenAndServe(); err != nil {
+		sentry.CaptureMessage("err")
+		log.Errorln("failed to start the server: ", err)
+	}
+}
